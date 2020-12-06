0973a01e0a0904dce0eed2a703c7d04cd3e121d4
Vladimir Terenya <vl.terenya@gmail.com>
2020-12-07 00:58:37 +0300

    Labs

--- Lab9/src/MyFrame.java
+++ Lab9/src/MyFrame.java
@@ -48,7 +48,7 @@ class MyFrame extends JFrame implements ActionListener {
             if (functionCalculation.isSelected()) {
                 double index = liner.calculationElemnt(Integer.parseInt(inputIndexField.getText()));
                 outField.setText(String.valueOf(index));
-        } else if (functionSum.isSelected()) {
+            } else if (functionSum.isSelected()) {
                 outField.setText(String.valueOf(liner.sumProgression()));
             } else if (functionSave.isSelected()) {
                 try {
@@ -83,7 +83,7 @@ class MyFrame extends JFrame implements ActionListener {
 
     MyFrame(String name) {
         super(name);
-        setBounds(500, 150, 265, 380);
+        setBounds(500, 150, 305, 380);
         setResizable(false);
         setLayout(null);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
@@ -95,30 +95,30 @@ class MyFrame extends JFrame implements ActionListener {
         label3.setBounds(10, 50, 80, 12);
 
         field1 = new JTextField();
-        field1.setBounds(label1.getX() + 80, label1.getY(), 150, 17);
+        field1.setBounds(label1.getX() + 80, label1.getY(), 200, 17);
 
         field2 = new JTextField();
-        field2.setBounds(label2.getX() + 80, label2.getY(), 150, 17);
+        field2.setBounds(label2.getX() + 80, label2.getY(), 200, 17);
 
         field3 = new JTextField();
-        field3.setBounds(label3.getX() + 80, label3.getY(), 150, 17);
+        field3.setBounds(label3.getX() + 80, label3.getY(), 200, 17);
 
         firstRequire = new JLabel("Choose type of series: ");
-        firstRequire.setBounds(label3.getX(), label3.getY() + 30, 130, 17);
+        firstRequire.setBounds(label3.getX(), label3.getY() + 30, 170, 17);
 
         secondRequire = new JLabel("Choose function(s): ");
-        secondRequire.setBounds(firstRequire.getX(), firstRequire.getY() + 30, 130, 17);
+        secondRequire.setBounds(firstRequire.getX(), firstRequire.getY() + 30, 160, 17);
 
         comboBoxSeries = new JComboBox(series);
-        comboBoxSeries.setBounds(firstRequire.getX() + firstRequire.getWidth(), firstRequire.getY(), 100, 17);
+        comboBoxSeries.setBounds(firstRequire.getX() + firstRequire.getWidth()+10, firstRequire.getY(), 100, 17);
         comboBoxSeries.setSelectedIndex(0);
 
         ButtonGroup buttonGroup = new ButtonGroup();
         functionCalculation = new JRadioButton("Calculation");
-        functionCalculation.setBounds(secondRequire.getX() + 120, secondRequire.getY(), 100, 17);
+        functionCalculation.setBounds(secondRequire.getX() + 150, secondRequire.getY(), 120, 17);
 
         inputIndexField = new JTextField();
-        inputIndexField.setBounds(functionCalculation.getX() + 20, functionCalculation.getY() + 20, 80, 17);
+        inputIndexField.setBounds(functionCalculation.getX() + 20, functionCalculation.getY() + 20, 100, 17);
 
         functionSum = new JRadioButton("Sum of series");
         functionSum.setBounds(functionCalculation.getX(), functionCalculation.getY() + 40, 120, 17);
